5dc3debd2dfcb4acb72317ea4f19756371ba2537
Japa_cama <al.2009.andre@gmail.com>
2025-10-10 17:53:02 -0300

    teste carrocel

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// animação de carrossel automático
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+let index = 0;
 
+function moveCarousel() {
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  const moveAmount = slides[index].offsetWidth * index;
+  track.style.transform = `translateX(-${moveAmount}px)`;
+}
+
+// muda o aluno a cada 3 segundos
+setInterval(moveCarousel, 3000);
